bc981e0e260af2bae21f933be38d0cf3948cd985
ControlAdmin360 <pabreu5601@gmail.com>
2026-09-01 21:20:54 -0500

    gas2

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ function cerrarSesion(forceReload = false) {
 
 //  SWICHT URL PARA CONEXION pública de tu Web App en: GAS / VERCEL 'COEXION AL BACKEND'
 //const GAS_API_URL = "https://backend-zeta-coral-88.vercel.app/api/rpc";
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzbjt3pYSoShv-eWWvLFbFSrGOo_NirsHA1gslQ7TUrgB9l1bSIvboB7HSIX3CtMRp6uQ/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwQBWLOd6FhwuQqGc9nCxd1oU9sVEX4EdnDkhFdph_cYX17nvL4WK0JmHTUEKuNKV9f0A/exec";
 
 // --- Motor Principal netRun (Conexión Directa e Híbrida a Apps Script) ---
 window.netRun = function () {
