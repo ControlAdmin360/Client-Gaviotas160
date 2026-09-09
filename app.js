31c3f3c32a894a82acd65bf2ef6eb04335138978
ControlAdmin360 <pabreu5601@gmail.com>
2026-09-09 15:02:41 -0500

    hkhk

--- app.js
+++ app.js
@@ -2790,7 +2790,7 @@ async function validarYGuardarExon() {
 
   // Mensajes de confirmación
   let mensajeAdvertencia = "⚠️ TENGA EN CUENTA QUE ESTA OPCIÓN NO PODRÁ DESHACERSE\n → Ud como operador asume la responsabilidad de ejecutar esta acción \n y contar con previa autorización para tal procedimiento."
-  let mensajeConfirm = `¿Confirma que decea registrar la operación para el Departamento ${depa}? \n\n ${mensajeAdvertencia}`;
+  let mensajeConfirm = `❓ ¿Confirma que desea registrar la operación para el Departamento ${depa} \n → Ud como operador asume la responsabilidad de ejecutar esta acción \n y contar con previa autorización para tal procedimiento.`;
    if (chkElim) {
     mensajeConfirm = `❓ ¿Está seguro de ANULAR y eliminar la exoneración activa del Departamento ${depa}?`;
   } else if (chkMorTot && chkMulTot) {
